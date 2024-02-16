5b3592d157021e9745dee2fb2070030c5ba81f67
Lev Rosenberg <lev.rosenberg@gmail.com>
2024-02-16 17:42:51 -0600

    resume download

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,19 +2,28 @@
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { Context } from "../context";
 
 export default function NavBar() {
   const pathname = usePathname();
-  const { state, dispatch } = useContext(Context);
-
+  const { dispatch } = useContext(Context);
+  const [width, setWidth] = useState(0);
   useEffect(() => {
     dispatch({
       type: "SET_PROJECT",
       payload: { projectUrl: "", projectTitle: "" },
     });
   }, [dispatch, pathname]);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <nav>
@@ -37,13 +46,25 @@ export default function NavBar() {
             Projects
           </Link>
         </li>
-        <li className="only-desktop">
-          <Link
-            className={`p-underline ${pathname === "/Resume" ? "active" : ""}`}
-            href="/Resume"
-          >
-            Resume
-          </Link>
+        <li>
+          {width > 768 ? (
+            <Link
+              className={`p-underline ${
+                pathname === "/Resume" ? "active" : ""
+              }`}
+              href="/Resume"
+            >
+              Resume
+            </Link>
+          ) : (
+            <a
+              href="/portfolio_resume.pdf"
+              download="LevsPortfolio"
+              target="_blank"
+            >
+              Resume
+            </a>
+          )}
         </li>
         <li>
           <Link
