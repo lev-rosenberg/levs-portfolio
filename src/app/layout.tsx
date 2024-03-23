5008be8f2eecdeb7ae03dd4edc654f46a1e24038
Lev Rosenberg <lev.rosenberg@gmail.com>
2024-03-23 11:41:46 -0400

    mobile fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ContextProvider>{children}</ContextProvider>
+        <div className="full-screen-mobile">
+          <ContextProvider>{children}</ContextProvider>
+        </div>
       </body>
     </html>
   );
