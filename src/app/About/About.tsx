c8e766c988a6f0e4a11c0a09eac2a4096de21d20
Lev Rosenberg <lev.rosenberg@gmail.com>
2024-03-25 15:51:25 -0500

    no bold in about

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -15,28 +15,23 @@ export default function About() {
       <h2>Lev the Engineer</h2>
       <div className="left-border">
         <p>
-          I am a software engineer who enjoys <strong>wearing many hats</strong>
-          . I relish in the creativity and design that comes with frontend
-          development, the systems thinking required of backend work, and the
-          balance of technical expertise and user empathy found on the product
-          side.
+          I am a software engineer who enjoys wearing many hats . I relish in
+          the creativity and design that comes with frontend development, the
+          systems thinking required of backend work, and the balance of
+          technical expertise and user empathy found on the product side.
         </p>
         <br />
         <p>
-          Most of all, I am a <strong>lover of learning</strong>. A double major
-          in computer science and cognitive science, I refuse to limit the
-          breadth of my interests––whether that be CS, linguistics,
-          neuroscience, politcs, history, cooking, art, music, or really
-          anything you throw at me!
+          Most of all, I am a lover of learning. A double major in computer
+          science and cognitive science, I refuse to limit the breadth of my
+          interests––whether that be CS, linguistics, neuroscience, politcs,
+          history, cooking, art, music, or really anything you throw at me!
         </p>
         <br />
         <p>
-          I&apos;m currently{" "}
-          <strong>
-            looking for full time full stack and/or frontend roles
-          </strong>{" "}
-          starting in September 2024. If you think I&apos;d be a good fit for
-          your company, please reach out at{" "}
+          I&apos;m currently looking for full time full stack and/or frontend
+          roles, especially in the startup realm. If you think I&apos;d be a
+          good fit for your company, please reach out at{" "}
           <a href="mailto:lev.rosenberg@gmail.com" className="underline">
             lev.rosenberg@gmail.com
           </a>{" "}
@@ -56,19 +51,18 @@ export default function About() {
         </p>
         <br />
         <p>
-          At ADAMAH Ceramics, each piece is a unique{" "}
-          <strong> blend of sculptural creativity and practicality</strong>.
-          Whether it&apos;s a chalice with a hand-shaped stem or a teapot shaped
-          from an unconventional donut-inspired form, I strive to make art an
-          integral part of your daily life.
+          At ADAMAH Ceramics, each piece is a unique blend of sculptural
+          creativity and practicality. Whether it&apos;s a chalice with a
+          hand-shaped stem or a teapot shaped from an unconventional
+          donut-inspired form, I strive to make art an integral part of your
+          daily life.
         </p>
         <br />
         <p>
           Over the past 8 years of arranging vocal music, I&apos; learned to
-          make each arrangement subvert expectations and bring{" "}
-          <strong>a new perspective to popular music</strong>. I love the
-          challenge of taking a song everyone knows and figuring out how to make
-          it my own.
+          make each arrangement subvert expectations and bring a new perspective
+          to popular music. I love the challenge of taking a song everyone knows
+          and figuring out how to make it my own.
         </p>
       </div>
     </div>
