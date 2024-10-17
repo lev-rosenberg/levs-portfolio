467da11e929385f524b076572c9705aeb2a450a6
Lev Rosenberg <lev.rosenberg@gmail.com>
2024-10-16 21:30:05 -0400

    about

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -17,68 +17,62 @@ export default function About() {
         <p>
           Software engineering came at me from left field. During my freshman
           year, a friend suggested I take an intro to computer science class. I
-          had never touched code before, but my curiosity got the better of me,
-          and I decided to give it a shot. Immediately, I fell in love,
-          captivated by the problem solving, the creativity, the balance between
-          big-picture concepts and low-level details, and all of software&apos;
-          broad applications in the world. I quickly declared my CS major (later
-          to compliment it with cognitive science).
+          had never touched code before, but I decided to give it a shot.
+          Immediately, I fell in love, captivated by the problem solving, the
+          balance between big-picture systems and low-level details, and all of
+          software&apos;s broad applications. I quickly declared my CS major.
         </p>
         <br />
         <p>
-          I easily found an outlet for creativity and design on the frontend
-          side. My first couple professional roles were both frontend focused.
-          But meanwhile I was always curious about what goes on behind the
-          scenes. And as soon as I discovered backend work, I was hooked by the
-          systems-thinking it engages. Soon, I started whipping up full stack
-          apps in my free time. Bridging the gap between the technical and the
-          human, rough some awesome classes at Northwestern (shoutout DTR), I
-          developed a deep appreciation for human-computer interaction. I
-          realized that figuring out how people think and behave is both
-          fascinating and necessary if you want to build solutions that resonate
-          with real users.
+          After learning the fundamentals, I found an outlet for creativity and
+          design on the frontend side––my first couple professional roles were
+          both frontend focused. But meanwhile I was always curious about what
+          goes on behind the scenes. And as soon as I was introduced backend
+          work, I was hooked by the systems-thinking involved. Bridging the gap
+          between the technical and the human, I developed a deep appreciation
+          for human-computer interaction through some awesome classes at
+          Northwestern (shoutout DTR). I realized that figuring out how people
+          work is both fascinating and necessary if you want to build solutions
+          that resonate with real users.
         </p>
       </div>
       <br />
       <h2>The Artist</h2>
       <div className="left-border">
         <p>
-          Beyond the technical, I spend most of my time making music and
-          pottery. I&apos;m a singer and pianist, but favorite part of music is
-          arranging it. Drilling deep on the details of specific chords and
-          voicings, but also pulling back and architecting the rise and fall of
-          a song. Being creative and subverting expectations. And always
-          thinking about the people I&apos;m arranging for. What are their
-          strengths and weaknesses as musicians and how can I craft an
-          arrangement that fits the group perfectly.
-        </p>{" "}
+          Beyond software, I spend most of my time making music and pottery.
+          I&apos;m a singer and pianist, but my favorite part of music is
+          arranging it––architecting the rise and fall of a piece but also
+          drilling deep on the details of specific chords and voicings. And
+          throughout the process, the musicians I&apos;m arranging for remain
+          top of mind so that I can craft an arrangement that fits the group
+          perfectly and plays to everyone&apos;s strengths.
+        </p>
         <br />
         <p>
           As a potter, I make functional work––I want to make things that people
-          will actually use. But that’s not to say there&apos;s no room for
-          artistry. Whether it&apos;s a mug with a hand-shaped base or a teapot
-          shaped like a torus form, my favorite part of ceramics is making art
-          and beauty an integral part of daily life.
+          will actually use. But that&apos;s not to say there&apos;s no room for
+          artistry. Whether it&apos;s a mug with a hand-shaped base or a
+          donut-shaped teapot, my favorite part of ceramics is making art an
+          integral part of daily life.
         </p>
       </div>
       <br />
       <h2>The Generalist</h2>
       <div className="left-border">
         <p>
-          What connects my work across different fields is a focus on people.
-          Whether I&apos;m developing software, arranging music, or crafting
-          pottery, I&apos;m driven by the desire to create experiences that
-          resonate. I believe great solutions, in any domain, start with
-          understanding who they&apos;re for.
-        </p>
-        <br />
-        <p>
-          I&apos;m a lifelong learner. I recieved degrees in Computer Science
+          I&apos;m a lifelong learner. I received degrees in Computer Science
           and Cognitive Science from Northwestern, but my curiosity extends to
           linguistics, neuroscience, sustainability, history, and more. I&apos;m
           always seeking to broaden my understanding of the world, and I love
-          finding ways to bring that knowledge into my work to make a meaningful
-          impact.
+          finding ways to apply that knowledge towards meaningful impact.
+        </p>
+        <br />
+        <p>
+          What connects my work across disciplines is a focus on people. Whether
+          I&apos;m developing software, arranging music, or crafting pottery, I
+          believe great solutions, in any domain, start with understanding who
+          they&apos;re for.
         </p>
       </div>
     </div>
